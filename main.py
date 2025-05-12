ffd4f97c8eacd87b454420499bf28057c8c2582c
Felipe-Silva7 <149840884+Felipe-Silva7@users.noreply.github.com>
2025-05-12 11:45:19 -0300

    conexão com firestore

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
+# Carrega as credenciais
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+# Inicializa o Firestore
+db = firestore.client()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursos_ref = db.collection("cursos")
+    docs = cursos_ref.stream()
+    cursos = [{**doc.to_dict(), "id": doc.id} for doc in docs] 
+    return render_template("index.html", cursos=cursos)
+
+@app.route("/curso/<curso_id>")
+def detalhes_curso(curso_id):
+    doc_ref = db.collection("cursos").document(curso_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        curso = doc.to_dict()
+        return render_template("curso_detalhes.html", curso=curso, curso_id=curso_id)
+    else:
+        return "Curso não encontrado", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
